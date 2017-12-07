47cbf21df331cf2a503525a96b07b610bd7841d9
Andrea Baccega <vekexasia@gmail.com>
2017-12-07 13:41:55 +0100

    Some cleanup

--- src/ui_elements_s.c
+++ src/ui_elements_s.c
@@ -1,14 +1,20 @@
-//
-// Created by andrea on 02/11/17.
-//
-
 #include "os_io_seproxyhal.h"
 
 const ux_menu_entry_t menu_main[];
 const ux_menu_entry_t menu_about[];
 
+/**
+ * TEMP global var used to print a lot of data (50chars)
+ */
 char linesBuffer[50];
+/**
+ * Actually used in the display.
+ */
 char lineBuffer[11];
+/**
+ * Coupled with linesBuffer its used to know how long is the buffer to print
+ * and split into chunks.
+ */
 uint8_t lineBufferLength = 0;
 
 const ux_menu_entry_t menu_main[] = {
@@ -23,29 +29,31 @@ const ux_menu_entry_t menu_about[] = {
   UX_MENU_END
 };
 
+#define CLEAN_SCREEN { {BAGL_RECTANGLE, 0x00, 0, 0, 128, 32, 0, 0, BAGL_FILL, 0x000000, 0xFFFFFF, 0, 0}, NULL, 0, 0, 0, NULL, NULL, NULL}
 #define ICON_CROSS { {BAGL_ICON, 0x00, 3, 12, 7, 7,  0, 0, 0, 0xFFFFFF, 0x000000, 0, BAGL_GLYPH_ICON_CROSS}, NULL, 0, 0, 0, NULL, NULL, NULL }
 #define ICON_CHECK { {BAGL_ICON, 0x00, 117, 13, 8, 6,  0, 0, 0, 0xFFFFFF, 0x000000, 0, BAGL_GLYPH_ICON_CHECK}, NULL, 0, 0, 0, NULL, NULL, NULL }
 #define ICON_DOWN  { {BAGL_ICON, 0x00, 117, 13, 8, 6,  0, 0, 0, 0xFFFFFF, 0x000000, 0, BAGL_GLYPH_ICON_DOWN}, NULL, 0, 0, 0, NULL, NULL, NULL }
 
+/**
+ * Sign with address
+ */
 const bagl_element_t bagl_ui_approval_nanos[] = {
+  CLEAN_SCREEN,
   {
-    {BAGL_RECTANGLE, 0x00, 0, 0, 128, 32, 0, 0, BAGL_FILL, 0x000000, 0xFFFFFF, 0, 0},
-    NULL, 0, 0, 0, NULL, NULL, NULL,
-  },
-  {
-    {BAGL_LABELINE,  0x02, 0, 12, 128, 11, 0, 0, 0, 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_REGULAR_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
+    {BAGL_LABELINE,  0x02, 0,   12, 128, 11, 0, 0, 0, 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_REGULAR_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
     "Sign with", 0, 0, 0, NULL, NULL, NULL,
   },
-
   {
-    {BAGL_LABELINE,  0x02, 23,  26, 82, 11,  0, 0, 0, 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 26},
+    {BAGL_LABELINE, 0x02, 23, 26, 82, 11, 0x80 | 10, 0, 0, 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 26},
     lineBuffer, 0, 0, 0, NULL, NULL, NULL,
   },
   ICON_CHECK,
-  ICON_CROSS
+  ICON_CROSS,
 };
 
-
+/**
+ * Review text to sign (message)
+ */
 const bagl_element_t bagl_ui_text_review_nanos[] = {
   // {
   //     {type, userid, x, y, width, height, stroke, radius, fill, fgcolor,
@@ -58,10 +66,7 @@ const bagl_element_t bagl_ui_text_review_nanos[] = {
   //     out,
   //     over,
   // },
-  {
-    {BAGL_RECTANGLE, 0x00, 0,   0,  128, 32, 0, 0, BAGL_FILL, 0x000000, 0xFFFFFF, 0, 0},
-    NULL, 0, 0, 0, NULL, NULL, NULL,
-  },
+  CLEAN_SCREEN,
   {
     {BAGL_LABELINE,  0x02, 0,   12, 128, 11, 0, 0, 0, 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_REGULAR_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
     "Verify text", 0, 0, 0, NULL, NULL, NULL,
